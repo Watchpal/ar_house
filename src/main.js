67fe689fc439b95597614eb956f5dcd0e1baa994
Watchpal <andrey.odegov.502@gmail.com>
2026-04-20 13:42:12 +0200

    Refactor camera settings and enhance GPS handling
    Updated camera perspective and added device orientation controls for improved user experience. Enhanced GPS handling and added dynamic box generation based on location updates.

--- src/main.js
+++ src/main.js
@@ -5,21 +5,22 @@ import heroImg from "./assets/hero.png";
 import * as THREE from 'three';
 import * as LocAR from 'locar';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
+const camera = new THREE.PerspectiveCamera(80, window.innerWidth / window.innerHeight, 0.001, 1000);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener("resize", e => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;    
-    camera.updateProjectionMatrix();
-});
-const box = new THREE.BoxGeometry(2,2,2);
-const cube = new THREE.Mesh(box, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+const scene = new THREE.Scene();
 
 const locar = new LocAR.LocationBased(scene, camera);
+
+window.addEventListener("resize", e => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
 
 const cam = new LocAR.Webcam({
     video: {
@@ -35,13 +36,69 @@ cam.on("webcamerror", error => {
     alert(`Webcam error: code ${error.code} message ${error.message}`);
 });
 
+let firstLocation = true;
+
+const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
+
+deviceOrientationControls.on("deviceorientationgranted", ev => {
+    ev.target.connect();
+});
+
+deviceOrientationControls.on("deviceorientationerror", error => {
+    alert(`Device orientation error: code ${error.code} message ${error.message}`);
+});
+
+deviceOrientationControls.init();
+
+locar.on("gpserror", error => {
+    alert(`GPS error: ${error.code}`);
+});
+
+locar.on("gpsupdate", ev => {
+    if(firstLocation) {
+
+        const boxProps = [{
+            latDis: 0.0005,
+            lonDis: 0,
+            colour: 0xff0000
+        }, {
+            latDis: -0.0005,
+            lonDis: 0,
+            colour: 0xffff00
+        }, {
+            latDis: 0,
+            lonDis: -0.0005,
+            colour: 0x00ffff
+        }, {
+            latDis: 0,
+            lonDis: 0.0005,
+            colour: 0x00ff00
+        }];
+
+        const geom = new THREE.BoxGeometry(10,10,10);
+
+        for(const boxProp of boxProps) {
+            const mesh = new THREE.Mesh(
+                geom, 
+                new THREE.MeshBasicMaterial({color: boxProp.colour})
+            );
+        
+            locar.add(
+                mesh, 
+                ev.position.coords.longitude + boxProp.lonDis, 
+                ev.position.coords.latitude + boxProp.latDis
+            );
+        }
+        
+        firstLocation = false;
+    }
+});
 
 locar.startGps();
-locar.add(cube, -0.72, 51.0501);
 
 renderer.setAnimationLoop(animate);
 
-
 function animate() {
+    deviceOrientationControls.update();
     renderer.render(scene, camera);
 }
